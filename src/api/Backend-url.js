df2ad51985488e9454e765807279581593ecc59a
Anand R P <anandrp.sics@gmail.com>
2024-04-19 18:05:32 +0530

    img issue fixed

--- src/api/Backend-url.js
+++ src/api/Backend-url.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://hybrid.srishticampus.in:4014/";
-// const BASE_URL = "http://localhost:4014/community_connect_api/";
+// const BASE_URL = "http://hybrid.srishticampus.in:4014/";
+const BASE_URL = "http://localhost:4014/";
 export default BASE_URL;
